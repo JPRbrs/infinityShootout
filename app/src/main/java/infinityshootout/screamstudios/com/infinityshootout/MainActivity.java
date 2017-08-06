ca60ec42bed702c30051f664eca58dd46de3e816
Javier# <perezrubio.javier@gmail.com>
2017-08-06 21:07:23 +0100

    Improve troop list item

--- app/src/main/java/infinityshootout/screamstudios/com/infinityshootout/MainActivity.java
+++ app/src/main/java/infinityshootout/screamstudios/com/infinityshootout/MainActivity.java
@@ -1,8 +1,10 @@
 package infinityshootout.screamstudios.com.infinityshootout;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,60 +26,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final ListView listView =(ListView) findViewById(R.id.listview);
-        String[] values = new String[] {"Android", "iPhone", "Windows Mobile"};
-
-        final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; i++) {
-            list.add(values[i]);
-        }
+        String[] values = new String[] {"Android", "iPhone", "caca"};
+//
+//        final ArrayList<String> list = new ArrayList<String>();
+//        for (int i = 0; i < values.length; i++) {
+//            list.add(values[i]);
+//        }
         final MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(
                 this,
                 values);
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener(
-                new AdapterView.OnItemClickListener()
-                {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, final View view, int position, long id){
-                        final String item = (String) parent.getItemAtPosition(position);
-                        view.animate().setDuration(2000).alpha(0).withEndAction(new Runnable() {
-                            @Override
-                            public void run() {
-                                list.remove(item);
-                                adapter.notifyDataSetChanged();
-                                view.setAlpha(1);
-                            }
-                        });
-                    }
-        });
+
     }
 
     public class MySimpleArrayAdapter extends ArrayAdapter<String>{
         private final Context context;
-        private final String[] values;
 
         public MySimpleArrayAdapter(Context context, String[] values) {
             super(context, -1, values);
             this.context = context;
-            this.values = values;
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater inflater = (LayoutInflater) context
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View rowView = inflater.inflate(R.layout.rowlayout, parent, false);
-//            Spinner weapon = (Spinner)findViewById(R.id.weapons);
-//            Spinner bs = (Spinner) findViewById(R.id.bs);
-//            TextView modifiersTV = (TextView) findViewById(R.id.modifiersTV);
-//            SeekBar modifiersSB = (SeekBar) findViewById(R.id.modifiersSB);
-//
-//            String[] weapons = new String[] {"HMG", "Combi Rifle", "Light flamethrower", "Grenades"};
-//            ArrayAdapter<String> weapons_adapter = new ArrayAdapter<String>(
-//                    this,
-//                    android.R.layout.simple_spinner_dropdown_item,
-//                    weapons);
-//            weapon.setAdapter(weapons_adapter);
+                LayoutInflater inflater = (LayoutInflater) context
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View rowView = inflater.inflate(R.layout.rowlayout, parent, false);
+                Spinner weapon = rowView.findViewById(R.id.weapons_spinner);
+
+                String[] weapons = new String[] {"HMG", "Combi Rifle", "Light flamethrower", "Grenades"};
+                ArrayAdapter<String> weapons_adapter = new ArrayAdapter<String>(
+                        context,
+                        android.R.layout.simple_spinner_dropdown_item,
+                        weapons);
+                weapon.setAdapter(weapons_adapter);
+
+                String[] possible_bs = new String[] {"10", "11", "12", "13", "15", "16"};
+                Spinner bs_spinner = rowView.findViewById(R.id.bs_spinner);
+                ArrayAdapter<String> bs_adapter = new ArrayAdapter<String>(
+                        context,
+                        android.R.layout.simple_spinner_dropdown_item,
+                        possible_bs);
+                bs_spinner.setAdapter(bs_adapter);
+
+            // rowView.setBackgroundColor(Color.RED);
+
+
+
 
             return rowView;
         }
